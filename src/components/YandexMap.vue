93f9a151cb6de43ad561b916468fcad6027f3e75
evanstcom <zhenyakoks@gmail.com>
2024-09-24 15:42:41 +0300

    fix ymaps

--- src/components/YandexMap.vue
+++ src/components/YandexMap.vue
@@ -1,53 +1,53 @@
-<!-- <template>
+<template>
   <div id="map" style="width: 100%; height: 400px"></div>
-</template> -->
-<!-- <script setup>
-    import { inject, onMounted } from 'vue'
-    const coords = inject('coords')
-    
-    onMounted(() => {
-      // Проверяем, загружен ли уже скрипт Yandex Maps API
-      if (
-        !document.querySelector(
-          'script[src="https://api-maps.yandex.ru/2.1/?lang=ru_RU&apikey=3abb8697-040a-4691-86e4-90f498afff44"]'
-        )
-      ) {
-        const script = document.createElement('script')
-        script.src =
-          'https://api-maps.yandex.ru/2.1/?lang=ru_RU&apikey=3abb8697-040a-4691-86e4-90f498afff44'
-        script.async = true
-    
-        script.onload = () => {
-          ymaps.ready(initMap)
-        }
-    
-        document.head.appendChild(script)
-      } else {
-        // Если скрипт уже загружен, просто вызываем инициализацию карты
-        ymaps.ready(initMap)
-      }
-    })
-    
-    const initMap = () => {
-      // Инициализация карты
-      console.log(coords.lat, coords.lon)
-      const myMap = new ymaps.Map('map', {
-        center: [coords.lat, coords.lon], // Координаты центра карты
-        zoom: 9,
-        mode: 'vector'
-      })
-    
-      // Добавление метки на карту
-      const placemark = new ymaps.Placemark([coords.lat, coords.lon], {
-        hintContent: 'Я здесь!',
-        balloonContent: 'Это красивая метка'
-      })
-    
-      myMap.geoObjects.add(placemark)
-    }
-    </script>
- -->
+</template>
 <script setup>
+import { inject, onMounted } from 'vue'
+const coords = inject('coords')
+
+onMounted(() => {
+  // Проверяем, загружен ли уже скрипт Yandex Maps API
+  if (
+    !document.querySelector(
+      'script[src="https://api-maps.yandex.ru/2.1/?lang=ru_RU&apikey=3abb8697-040a-4691-86e4-90f498afff44"]'
+    )
+  ) {
+    const script = document.createElement('script')
+    script.src =
+      'https://api-maps.yandex.ru/2.1/?lang=ru_RU&apikey=3abb8697-040a-4691-86e4-90f498afff44'
+    script.async = true
+
+    script.onload = () => {
+      ymaps.ready(initMap)
+    }
+
+    document.head.appendChild(script)
+  } else {
+    // Если скрипт уже загружен, просто вызываем инициализацию карты
+    ymaps.ready(initMap)
+  }
+})
+
+const initMap = () => {
+  // Инициализация карты
+  console.log(coords.lat, coords.lon)
+  const myMap = new ymaps.Map('map', {
+    center: [coords.lat, coords.lon], // Координаты центра карты
+    zoom: 9,
+    mode: 'vector'
+  })
+
+  // Добавление метки на карту
+  const placemark = new ymaps.Placemark([coords.lat, coords.lon], {
+    hintContent: 'Я здесь!',
+    balloonContent: 'Это красивая метка'
+  })
+
+  myMap.geoObjects.add(placemark)
+}
+</script>
+
+<!-- <script setup>
 onMounted(() => {
   // Проверяем, загружен ли уже скрипт Yandex Maps API
   if (
@@ -83,4 +83,4 @@ const LOCATION = {
       </YMapMarker>
     </YMap>
   </div>
-</template>
+</template> -->
