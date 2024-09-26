70af9b048dd1c1f4aee50672cfa65c18e0f794a6
evanstcom <zhenyakoks@gmail.com>
2024-09-26 13:06:30 +0300

    fix image

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,11 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/sneakers/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      vue: 'vue/dist/vue.esm-bundler.js'
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: '/sneakers/'
 })
