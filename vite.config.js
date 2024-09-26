75e7c44014d537a0764f1cfd9f495020f9fefe95
evanstcom <zhenyakoks@gmail.com>
2024-09-26 12:37:54 +0300

    add base on vite config

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
   base: '/sneakers/',
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
